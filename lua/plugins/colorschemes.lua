25642075d2659ee34c4db64917e808aba348b52d
n0op3 <n0op3@proton.me>
2025-08-15 17:19:44 +0200

    MORE THEMEEEEES

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,11 +1,14 @@
 return {
-    { "folke/tokyonight.nvim",       name = "Tokyonight Color Scheme" },
-    { "Shatur/neovim-ayu",           name = "Ayu Color Scheme" },
-    { "catppuccin/nvim",             name = "Catppuccin Color Scheme" },
-    { "projekt0n/github-nvim-theme", name = "GitHub Color Scheme" },
-    { "rebelot/kanagawa.nvim",       name = "Kanagawa Color Scheme" },
-    { "rose-pine/neovim",            name = "Rose Pine Color Scheme" },
-    { "ellisonleao/gruvbox.nvim",    name = "Gruvbox Color Scheme" },
-    { "Mofiqul/dracula.nvim",        name = "Dracula Color Scheme" },
-    { "doums/darcula",               name = "Darcula Color Scheme" }
+    { "folke/tokyonight.nvim",            name = "Tokyonight Color Scheme" },
+    { "Shatur/neovim-ayu",                name = "Ayu Color Scheme" },
+    { "catppuccin/nvim",                  name = "Catppuccin Color Scheme" },
+    { "projekt0n/github-nvim-theme",      name = "GitHub Color Scheme" },
+    { "rebelot/kanagawa.nvim",            name = "Kanagawa Color Scheme" },
+    { "rose-pine/neovim",                 name = "Rose Pine Color Scheme" },
+    { "ellisonleao/gruvbox.nvim",         name = "Gruvbox Color Scheme" },
+    { "Mofiqul/dracula.nvim",             name = "Dracula Color Scheme" },
+    { "doums/darcula",                    name = "Darcula Color Scheme" },
+    { "nyoom-engineering/oxocarbon.nvim", name = "Oxycarbox Color Scheme" },
+    { "scottmckendry/cyberdream.nvim",    name = "Cyberdream Color Scheme" },
+    { "olimorris/onedarkpro.nvim",        name = "OneDarkPro Color Scheme" }
 }
