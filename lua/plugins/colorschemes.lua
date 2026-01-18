16bae23bfdeaeec560fccb7de572dd3ac503cdca
n0op3 <n0op3@proton.me>
2026-01-18 20:25:48 +0100

    more themes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,3 +1,8 @@
 return {
-    { "catppuccin/nvim", name = "Catppuccin Color Scheme" }
+    { "catppuccin/nvim",                  name = "Catppuccin Color Scheme" },
+    { "rebelot/kanagawa.nvim",            name = "Kanagawa Color Scheme" },
+    { "rose-pine/neovim",                 name = "Rose Pine Color Scheme" },
+    { "ellisonleao/gruvbox.nvim",         name = "Gruvbox Color Scheme" },
+    { "olimorris/onedarkpro.nvim",        name = "OneDarkPro Color Scheme" },
+    { "nyoom-engineering/oxocarbon.nvim", name = "Oxycarbox Color Scheme" },
 }
