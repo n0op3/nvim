c663dc554dd823fcab30d88e4c1d7223376603cd
n0op3 <n0op3@proton.me>
2026-01-18 19:51:43 +0100

    more mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+local telescope = require("telescope.builtin")
 vim.g.mapleader = " "
 
 -- Core
@@ -9,23 +10,29 @@ map("n", ";", ":")
 -- Movement
 map("n", "j", "gj")
 map("n", "k", "gk")
+map("n", "<C-S-K>", vim.cmd.bnext)
+map("n", "<C-S-J>", vim.cmd.bprev)
+map("n", "<leader>x", vim.cmd.bdelete)
 
--- Telescope
-local builtin = require("telescope.builtin")
-map("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-map("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-map("n", "<leader>dx", builtin.diagnostics, { desc = "Telescope diagnostics" })
-map("n", "<leader>st", builtin.colorscheme, { desc = "Switch colorscheme" })
-map("n", "<leader>gf", builtin.live_grep, { desc = "Find string" })
-map("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-map("n", "<leader>fm", builtin.buffers, { desc = "Find marks" })
-map("n", "<leader>fr", builtin.buffers, { desc = "Find registers" })
-map("n", "gd", builtin.lsp_references, { desc = "LSP references" })
-map("n", "<leader>gl", builtin.git_commits, { desc = "Git log" })
-map("n", "<leader>gb", builtin.git_branches, { desc = "Git branches" })
-map("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
+-- Search
+map("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
+map("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
+map("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
+map("n", "<leader>gf", telescope.live_grep, { desc = "Find string" })
+map("n", "<leader>fb", telescope.buffers, { desc = "Find buffers" })
+map("n", "<leader>fm", telescope.marks, { desc = "Find marks" })
+map("n", "<leader>fr", telescope.registers, { desc = "Find registers" })
+
+-- LSP
+map("n", "<leader>rx", telescope.lsp_references, { desc = "LSP references" })
+map("n", "<leader>dx", telescope.diagnostics, { desc = "Telescope diagnostics" })
+
+-- Git
+map("n", "<leader>gl", telescope.git_commits, { desc = "Git log" })
+map("n", "<leader>gb", telescope.git_branches, { desc = "Git branches" })
+map("n", "<leader>gs", telescope.git_status, { desc = "Git status" })
 
 -- Misc
 map("v", "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to system clipboard" })
+map("n", "<leader>st", telescope.colorscheme, { desc = "Switch colorscheme" })
+map("n", "<esc>", vim.cmd.noh)
