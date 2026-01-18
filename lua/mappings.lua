833729b1f3450330b218a0fe761bd9e35f0aeabb
n0op3 <n0op3@proton.me>
2026-01-18 20:44:42 +0100

    toggle formatting bind

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -44,6 +44,7 @@ map("n", "<leader>ra", vim.lsp.buf.rename, { desc = "Rename occurences" })
 map("n", "<leader>ca", function() require("tiny-code-action").code_action() end, { desc = "Code actions" })
 map("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 map("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+map("n", "<leader>bf", "<CMD>ToggleFormatting<CR>", { desc = "Toggle LSP formatting for the current buffer" })
 
 -- Git
 map("n", "<leader>gl", telescope.git_commits, { desc = "Git log" })
