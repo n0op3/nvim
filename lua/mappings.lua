6ac603e891c8e52eaa11eae571925997d234ea48
n0op3 <n0op3@proton.me>
2026-01-18 17:12:40 +0100

    diagnostics

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,6 +16,7 @@ map("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 map("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+map("n", "<leader>dx", builtin.diagnostics, { desc = "Telescope diagnostics" })
 
 -- Misc
 map("v", "<leader>y", '"+y', { noremap = true, silent = true, desc = "Yank to system clipboard" })
