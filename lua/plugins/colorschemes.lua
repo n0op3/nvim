c71a56a9578c00f1029d77351c29e43b00639955
n0op3 <n0op3@proton.me>
2026-02-10 14:31:28 +0100

    koda color scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,10 +1,11 @@
 return {
-    { "catppuccin/nvim",                  name = "Catppuccin Color Scheme" },
-    { "rebelot/kanagawa.nvim",            name = "Kanagawa Color Scheme" },
-    { "rose-pine/neovim",                 name = "Rose Pine Color Scheme" },
-    { "ellisonleao/gruvbox.nvim",         name = "Gruvbox Color Scheme" },
-    { "olimorris/onedarkpro.nvim",        name = "OneDarkPro Color Scheme" },
-    { "nyoom-engineering/oxocarbon.nvim", name = "Oxycarbox Color Scheme" },
-    { "folke/tokyonight.nvim",            name = "Tokyonight Color Scheme" },
-    { "nanotech/jellybeans.vim",          name = "Jellybeans Color Scheme" }
+    { "catppuccin/nvim",                        name = "Catppuccin Color Scheme" },
+    { "rebelot/kanagawa.nvim",                  name = "Kanagawa Color Scheme" },
+    { "rose-pine/neovim",                       name = "Rose Pine Color Scheme" },
+    { "ellisonleao/gruvbox.nvim",               name = "Gruvbox Color Scheme" },
+    { "olimorris/onedarkpro.nvim",              name = "OneDarkPro Color Scheme" },
+    { "nyoom-engineering/oxocarbon.nvim",       name = "Oxycarbox Color Scheme" },
+    { "folke/tokyonight.nvim",                  name = "Tokyonight Color Scheme" },
+    { "nanotech/jellybeans.vim",                name = "Jellybeans Color Scheme" },
+    { "https://github.com/oskarnurm/koda.nvim", name = "Koda Color Scheme" }
 }
