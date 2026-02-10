980bb5c0d76bd6145d596ad1d2a08c39f54e8d5c
n0op3 <n0op3@proton.me>
2026-02-10 13:52:12 +0100

    jellybeans for that old school vibe~

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -6,4 +6,5 @@ return {
     { "olimorris/onedarkpro.nvim",        name = "OneDarkPro Color Scheme" },
     { "nyoom-engineering/oxocarbon.nvim", name = "Oxycarbox Color Scheme" },
     { "folke/tokyonight.nvim",            name = "Tokyonight Color Scheme" },
+    { "nanotech/jellybeans.vim",          name = "Jellybeans Color Scheme" }
 }
