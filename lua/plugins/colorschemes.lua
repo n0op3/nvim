2ff3f11074ee4c17319795395c586f6aacc516bd
n0op3 <n0op3@proton.me>
2026-04-02 14:23:09 +0200

    ashen color scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,11 +1,12 @@
 return {
-    { "catppuccin/nvim",                        name = "Catppuccin Color Scheme" },
-    { "rebelot/kanagawa.nvim",                  name = "Kanagawa Color Scheme" },
-    { "rose-pine/neovim",                       name = "Rose Pine Color Scheme" },
-    { "ellisonleao/gruvbox.nvim",               name = "Gruvbox Color Scheme" },
-    { "olimorris/onedarkpro.nvim",              name = "OneDarkPro Color Scheme" },
-    { "nyoom-engineering/oxocarbon.nvim",       name = "Oxycarbox Color Scheme" },
-    { "folke/tokyonight.nvim",                  name = "Tokyonight Color Scheme" },
-    { "nanotech/jellybeans.vim",                name = "Jellybeans Color Scheme" },
-    { "https://github.com/oskarnurm/koda.nvim", name = "Koda Color Scheme" }
+    { "catppuccin/nvim",                  name = "Catppuccin Color Scheme" },
+    { "rebelot/kanagawa.nvim",            name = "Kanagawa Color Scheme" },
+    { "rose-pine/neovim",                 name = "Rose Pine Color Scheme" },
+    { "ellisonleao/gruvbox.nvim",         name = "Gruvbox Color Scheme" },
+    { "olimorris/onedarkpro.nvim",        name = "OneDarkPro Color Scheme" },
+    { "nyoom-engineering/oxocarbon.nvim", name = "Oxycarbox Color Scheme" },
+    { "folke/tokyonight.nvim",            name = "Tokyonight Color Scheme" },
+    { "nanotech/jellybeans.vim",          name = "Jellybeans Color Scheme" },
+    { "oskarnurm/koda.nvim",              name = "Koda Color Scheme" },
+    { "ficd0/ashen.nvim",                 name = "Ashen Color Scheme" },
 }
