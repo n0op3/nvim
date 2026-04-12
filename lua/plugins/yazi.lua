c4357d0cf13b833a47842502f0d51a743d8fee90
n0op3 <n0op3@proton.me>
2026-04-12 18:28:42 +0200

    fix: disable yazi vertical split to allow multi line editing in neovim

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -25,17 +25,13 @@ return {
     },
     ---@type YaziConfig | {}
     opts = {
-        -- if you want to open yazi instead of netrw, see below for more info
         open_for_directories = true,
         keymaps = {
             show_help = "<f1>",
+            open_file_in_vertical_split = "<>",
         },
     },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
     init = function()
-        -- mark netrw as loaded so it's not loaded at all.
-        --
-        -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
         vim.g.loaded_netrwPlugin = 1
     end,
 }
