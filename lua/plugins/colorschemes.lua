284d00011329d25acd4a81969c9620f683f4a3e9
n0op3 <n0op3@proton.me>
2026-04-16 17:57:17 +0200

    feat: mellifluous color scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -10,4 +10,5 @@ return {
     { "oskarnurm/koda.nvim",              name = "Koda Color Scheme" },
     { "ficd0/ashen.nvim",                 name = "Ashen Color Scheme" },
     { "Shatur/neovim-ayu",                name = "Ayu Color Scheme" },
+    { "ramojus/mellifluous.nvim",         name = "Mellifluous Color Scheme" }
 }
