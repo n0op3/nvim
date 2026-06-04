e1156eccf5691336bd8546e2b4901a7e87dd87c1
n0op3 <n0op3@proton.me>
2026-06-04 21:25:46 +0200

    minimalize lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -23,78 +23,27 @@ return {
                         comp = { comp }
                         section[id] = comp
                     end
-                    comp.separator = left and { right = '' } or { left = '' }
+                    comp.separator = left and { right = '' } or { left = '' }
                 end
             end
             return sections
-        end
-
-        local function search_result()
-            if vim.v.hlsearch == 0 then
-                return ''
-            end
-            local last_search = vim.fn.getreg('/')
-            if not last_search or last_search == '' then
-                return ''
-            end
-            local searchcount = vim.fn.searchcount { maxcount = 9999 }
-            return last_search .. '(' .. searchcount.current .. '/' .. searchcount.total .. ')'
-        end
-
-        local function modified()
-            if vim.bo.modified then
-                return '+'
-            elseif vim.bo.modifiable == false or vim.bo.readonly == true then
-                return '-'
-            end
-            return ''
         end
 
         require('lualine').setup {
             options = {
                 theme = theme,
                 component_separators = '',
-                section_separators = { left = '', right = '' },
             },
             sections = process_sections {
-                lualine_a = { 'mode' },
+                lualine_a = {},
                 lualine_b = {
-                    'branch',
-                    'diff',
-                    {
-                        'diagnostics',
-                        source = { 'nvim' },
-                        sections = { 'error' },
-                    },
-                    {
-                        'diagnostics',
-                        source = { 'nvim' },
-                        sections = { 'warn' },
-                    },
                     { 'filename', file_status = false, path = 1 },
-                    {
-                        '%w',
-                        cond = function()
-                            return vim.wo.previewwindow
-                        end,
-                    },
-                    {
-                        '%r',
-                        cond = function()
-                            return vim.bo.readonly
-                        end,
-                    },
-                    {
-                        '%q',
-                        cond = function()
-                            return vim.bo.buftype == 'quickfix'
-                        end,
-                    },
+                    '%l:%c', '%p%%/%L'
                 },
                 lualine_c = {},
                 lualine_x = {},
-                lualine_y = { search_result, 'filetype' },
-                lualine_z = { '%l:%c', '%p%%/%L' },
+                lualine_y = {},
+                lualine_z = {},
             },
             inactive_sections = {
                 lualine_c = { '%f %y %m' },
