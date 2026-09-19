4a2c5d3b1e0b28b618bc5a4015d6fc7d24181407
n0op3 <n0op3@proton.me>
2026-09-19 20:31:14 +0200

    fix mappings to allow C-i to go forward in the jump list

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,7 +14,7 @@ map("v", ";", ":")
 map("n", "j", "gj")
 map("n", "k", "gk")
 map("n", "<C-n>", vim.cmd.bnext)
-map("n", "<C-i>", vim.cmd.bprev)
+map("n", "<C-m>", vim.cmd.bprev)
 map("n", "<leader>x", function()
     local bufs = vim.fn.getbufinfo({ buflisted = 1 })
     if #bufs > 1 then
